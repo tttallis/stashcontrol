ee15f078062522ee61a06d13a8728611d346cbaa
Thomas Ashelford <thomas.ashelford@protonmail.com>
2025-08-10 20:01:25 +1000

    admin

--- tracker/admin.py
+++ tracker/admin.py
@@ -1,11 +1,16 @@
 from django.contrib import admin
 from .models import Product, Batch, Container, Dose, Measurement, DeliveryMode
 
-# Register your models here.
-admin.site.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+	list_display = ('name', 'product_weight')
+
+class MeasurementAdmin(admin.ModelAdmin):
+	list_display = ('timestamp', 'container', 'weight')
+
+admin.site.register(Product, ProductAdmin)
 admin.site.register(Batch)
 admin.site.register(Container)
 admin.site.register(Dose)
-admin.site.register(Measurement)
+admin.site.register(Measurement, MeasurementAdmin)
 admin.site.register(DeliveryMode)
 
--- tracker/models.py
+++ tracker/models.py
@@ -8,14 +8,21 @@ class Product(models.Model):
 	product_weight = models.FloatField()
 	# product_type = models.ForeignKey() # for now, we assume it's flower
 	catalyst_id = models.UUIDField(blank=True, null=True)
-	# strain?
+	strain = models.TextField(blank=True)
+	
+	def __str__(self):
+		return self.name
 	
 class Batch(models.Model):
 	"""
 	A specific batch
 	"""
+	product = models.ForeignKey('Product', on_delete=models.CASCADE)
 	identifier = models.TextField()
-	# strain?
+	strain = models.TextField(blank=True)
+	
+	def __str__(self):
+		return self.identifier
 	
 class Container(models.Model):
 	"""
@@ -23,13 +30,16 @@ class Container(models.Model):
 	"""
 	product = models.ForeignKey('Product', on_delete=models.CASCADE)
 	batch = models.ForeignKey('Batch', blank=True, null=True, on_delete=models.SET_NULL)
-	dispensed = models.DateTimeField(blank=True, null=True)
+	dispensed = models.DateField(blank=True, null=True)
 	patient = models.ForeignKey('auth.User', on_delete=models.CASCADE)
-	opened = models.DateTimeField(blank=True, null=True)
-	finished = models.DateTimeField(blank=True, null=True)
+	opened = models.DateField(blank=True, null=True)
+	finished = models.DateField(blank=True, null=True)
 	include_container_weight = models.BooleanField(default=True, help_text="Is the container weight included in measurements?")
 	include_lid_weight = models.BooleanField(default=False, help_text="Is the lid weight included in measurements?")
 	include_humidity_pack_weight = models.BooleanField(default=True, help_text="Is a humidity pack weight included in measurements?")
+	
+	def __str__(self):
+		return f"{self.product.name} {self.pk}"
 	
 class Measurement(models.Model):
 	
@@ -37,10 +47,16 @@ class Measurement(models.Model):
 	timestamp = models.DateTimeField()
 	weight = models.FloatField()
 	
+	def __str__(self):
+		return f"{self.container.product.name} - {self.timestamp.strftime("%Y-%m-%d %H:%M:%S")}: {self.weight}g"
+	
 class DeliveryMode(models.Model):
 	name = models.CharField(max_length=100, blank=True)
 	user_defined = models.BooleanField()
 	user = models.ForeignKey('auth.User', blank=True, null=True, on_delete=models.CASCADE)
+	
+	def __str__(self):
+		return self.name
 	
 class Dose(models.Model):
 	container = models.ForeignKey('Container', on_delete=models.CASCADE)
